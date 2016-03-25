ef577f20bed2aa04f1d5fb7d1df4697b1ceab005
Tam Lu <tamlu907@gmail.com>
2016-03-24 20:00:57 -0700

    Added scripts, sound, scenes, fonts...

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Application.LoadLevel (name);
+	}
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	static MusicPlayer instance = null;
+	
+	void Start () {
+		if (instance != null) {
+			Destroy (gameObject); //destroys the duplicate that is created 
+		}
+		else {
+			instance = this; //instance is equal to the currently running song
+			GameObject.DontDestroyOnLoad (gameObject); //makes sure it doesn't get destroyed
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
