54b060ccd1a901ac7aabba8f2557734ce36f7a57
Tam Lu <tamlu907@gmail.com>
2016-03-26 01:37:13 -0700

    Added collision walls

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+	
+	void Start () {
+		paddleToBallVector = transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			//Lock the ball relative to the paddle
+			transform.position = paddle.transform.position + paddleToBallVector;
+			} 
+		if (Input.GetMouseButtonDown (0) && !hasStarted) {
+			//The game has started. Give the ball some velocity
+			hasStarted = true;
+			this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+		}
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+	public float mousePosInBlocks;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+		Vector3 paddlePos = new Vector3(Mathf.Clamp (mousePosInBlocks, 0.5f, 15.5f), 0.5f, 0f);
+		transform.position = paddlePos;
+
+	}
+}
+
+
+/*		MousePosition could also be done this way:
+		Vector3 paddlePos = newVector(0f, 0.5f, 0f);
+		paddlePos.x = Mathf.Clamp( Input.mousePosition.x / Screen.width * 16);
+		transform.position = paddlePos;
+*/
